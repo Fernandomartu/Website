e85c60f5f8b624415d7f2c29f60ef9a42123e19e
Fernando Marturet <fernando.martu@gmail.com>
2023-11-15 14:55:00 -0500

    fixing issue with unread variable

--- website-portfolio/src/App.tsx
+++ website-portfolio/src/App.tsx
@@ -10,15 +10,11 @@ function App() {
     SelectedPage.Home
   );
 
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
-        setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
       }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -26,11 +22,7 @@ function App() {
 
   return (
     <div className="app">
-      <Navbar
-        isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
 
       <Home setSelectedPage={setSelectedPage} />
       <Resume setSelectedPage={setSelectedPage} />
