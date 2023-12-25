de4866ab70210d041a3184ab99282c4c8d9744ab
Fernando Marturet <fernando.martu@gmail.com>
2023-12-24 21:45:38 -0500

    added gpteach project

--- website-portfolio/src/scenes/Home/index.tsx
+++ website-portfolio/src/scenes/Home/index.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "@/shared/types";
 import SocialMediaApp from "@/assets/Socialmediaapp.png";
 import visitorTracker from "@/assets/visitor-tracker.png";
 import gymApp from "@/assets/gymapp.png";
+import gpteach from "@/assets/gpteach.png";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -44,6 +45,84 @@ const Home = ({ setSelectedPage }: Props) => {
             Projects
           </h3>
           <div className="flex flex-col sm:px-5 px-0 md:gap-40 gap-20">
+            <div
+              onClick={
+                isAboveMediumScreens
+                  ? undefined
+                  : () => {
+                      if (!gitProjectTwoHovered) {
+                        handleProjectClick(
+                          "https://gpteach-frontend.onrender.com"
+                        );
+                      }
+                    }
+              }
+              className="md:flex relative hover:cursor-pointer hover:border-neon-50 md:hover:border-0 hover:border-2 transition-border"
+            >
+              <div className="flex flex-col md:relative absolute top-0 gap-5 md:items-start items-start md:left-5 md:mt-0 mt-5 px-5">
+                <h3 className="sm:text-3xl text-lg z-[30] font-bold">
+                  MERN Stack
+                </h3>
+                <h4 className="sm:text-2xl text-lg z-[30] font-bold">
+                  GPTeach
+                </h4>
+                <p className="md:p-5 md:bg-paragraph-50 sm:text-lg z-[30] font-bold">
+                  An AI powered tutor. Ask it about any topic and start
+                  learning. ChatGPT is integrated into a threejs animated robot.
+                  The AI is also instructed and given access to a CanvasJS
+                  whiteboard to draw and display text.
+                </p>
+                <div className="flex flex-wrap">
+                  <ul className="flex sm:text-xl text-neon-50 gap-5 z-[30] font-bold wrap">
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                  </ul>
+                  <ul className="flex sm:text-xl text-neon-50 gap-5 z-[30] font-bold wrap">
+                    <li>ThreeJS</li>
+                    <li>ChatGPT</li>
+                    <li>Typescript</li>
+                  </ul>
+                </div>
+
+                <a
+                  className="z-[30]"
+                  id="git-button-two"
+                  href="https://github.com/Fernandomartu/GPTeach"
+                  target="_blank"
+                  onMouseOver={() => setGitProjectTwoHovered(true)}
+                  onMouseLeave={() => setGitProjectTwoHovered(false)}
+                >
+                  <svg
+                    className="w-8 h-8 z-[30]"
+                    width="98"
+                    height="96"
+                    viewBox="0 0 98 96"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                      className={
+                        gitProjectTwoHovered ? "fill-neon-50" : "fill-white"
+                      }
+                    />
+                  </svg>
+                </a>
+              </div>
+              <div className="md:basis-2/3 min-w-[600px] min-h-[400px] max-h-[400px] relative md:right-5 overflow-hidden">
+                <a
+                  href="https://gpteach-frontend.onrender.com" // Replace with your desired external URL
+                  target="_blank" // Opens the link in a new tab
+                  rel="noopener noreferrer" // Recommended for security
+                >
+                  <img
+                    className="opacity-20 md:h-[300px] md:min-h-[300px] min-h-[400px] md:opacity-60 md:hover:opacity-100 transition-opacity cursor-pointer"
+                    src={gpteach}
+                  />
+                </a>
+              </div>
+            </div>
             <div
               onClick={
                 isAboveMediumScreens
